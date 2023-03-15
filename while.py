3655ee333bb4aba6f75375b50872c42958797550
sfrefsfref7 <119869292+sfrefsfref7@users.noreply.github.com>
2023-03-15 19:52:31 +0700

    Delete while.py

--- while.py
+++
@@ -1,6 +0,0 @@
-username = input("username : " )
-password = input("password : ")
-while username != "admin" or password != "1234":
-    username = input("username : " )
-    password = input("password : ")
-print("Done !")
